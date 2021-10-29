de5905a1db12e9ba98e68d2c1a4926dc21ac8d8b
wu.ting@ibm.com <wu.ting@ibm.com>
2021-10-30 00:37:21 +0200

    first commit

---
+++ tekton/pipelineRun.sh
@@ -0,0 +1,3 @@
+curl -X POST -d '{"head_commit": {"id": "HEAD"}, "repository": {"url": "https://github.com/fbh112/e2e_project.git", "name": "funfact-app"}}' \
+     -H "Content-Type: application/json" \
+     webhook-receiver-e2e-project.itzroks-100000kr1k-fz0n6p-6ccd7f378ae819553d37d5f2ee142bd6-0000.ams03.containers.appdomain.cloud
